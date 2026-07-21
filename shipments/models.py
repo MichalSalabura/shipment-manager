c913ec6451d6b249213db24607dd7909d53a5c20
MichalS <D00282723@student.dkit.ie>
2026-07-21 20:01:43 +0200

    feat: define package model

--- shipments/models.py
+++ shipments/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Package(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_package")
+    driver = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="assigned_package")
+    from_name = models.CharField(max_length=50)
+    from_address_line_1 = models.CharField(max_length=50)
+    from_address_line_2 = models.CharField(max_length=50, blank=True)
+    from_address_city = models.CharField(max_length=50)
+    from_address_region = models.CharField(max_length=50)
+    from_address_post_code = models.CharField(max_length=50)
+    from_country = models.CharField(max_length=50)
+    
+    to_name = models.CharField(max_length=50)
+    to_address_line_1 = models.CharField(max_length=50)
+    to_address_line_2 = models.CharField(max_length=50, blank=True)
+    to_address_city = models.CharField(max_length=50)
+    to_address_region = models.CharField(max_length=50)
+    to_address_post_code = models.CharField(max_length=50)
+    to_country = models.CharField(max_length=50)
+    recipient_phone = models.CharField(max_length=50)
+    recipient_email = models.EmailField()
+    scheduled_delivery = models.DateTimeField()
+    
+    width = models.FloatField()
+    height = models.FloatField()
+    depth = models.FloatField()
