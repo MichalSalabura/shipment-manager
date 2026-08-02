ad710e999f80892c76857704a838a7fde030a393
MichalS <D00282723@student.dkit.ie>
2026-08-03 01:10:55 +0200

    feat: add driver dashboard

--- shipments/urls.py
+++ shipments/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import UpdatePackageStatusView
-
+from .views import UpdatePackageStatusView, DriverDashboardView
 urlpatterns = [
+    path('driver/dashboard/', DriverDashboardView.as_view(), name='driver_dashboard'),
     path('packages/<str:pk>/status/', UpdatePackageStatusView.as_view(), name='update_package_status'),
 ]
--- shipments/views.py
+++ shipments/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import get_object_or_404
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Package, StatusHistory
 from .forms import PackageStatusUpdateForm
-
-
 # Create your views here.
 
 class UpdatePackageStatusView(LoginRequiredMixin, CreateView):
@@ -18,3 +16,13 @@ class UpdatePackageStatusView(LoginRequiredMixin, CreateView):
         form.instance.package = get_object_or_404(Package, pk = self.kwargs['pk'])
         form.instance.logged_by = self.request.user
         return super().form_valid(form)
+
+class DriverDashboardView(LoginRequiredMixin, ListView):
+    model = Package
+    template_name = "shipments/driver_dashboard.html"
+    context_object_name = "assigned_packages"
+
+    def get_queryset(self):
+        if hasattr(self.request.user, 'driver_profile'):
+            return Package.objects.filter(driver=self.request.user.driver_profile)
+        return Package.objects.none()
