667073a8bad9dc6ce4bf9a414f8a2cc8b9535c66
MichalS <D00282723@student.dkit.ie>
2026-08-03 00:24:13 +0200

    feat: create package status update view

--- shipments/views.py
+++ shipments/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Package, StatusHistory
+from .forms import PackageStatusUpdateForm
+
 
 # Create your views here.
+
+class UpdatePackageStatusView(LoginRequiredMixin, CreateView):
+    model = StatusHistory
+    form_class = PackageStatusUpdateForm
+    template_name = 'shipments/update_status.html'
+    success_url = reverse_lazy('driver_dashboard')
+
+    def form_valid(self, form):
+        form.instance.package = get_object_or_404(Package, pk = self.kwargs['pk'])
+        form.instance.logged_by = self.request.user
+        return super().form_valid(form)
