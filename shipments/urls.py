fd29f419d04fcda759b280d0e5b9080a4b3cc7eb
MichalS <D00282723@student.dkit.ie>
2026-08-03 02:06:35 +0200

    feat: create package creation template and add url

--- shipments/urls.py
+++ shipments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UpdatePackageStatusView, DriverDashboardView, ClientDashboardView
+from .views import UpdatePackageStatusView, DriverDashboardView, ClientDashboardView, CreatePackageView
 urlpatterns = [
     path('driver/dashboard/', DriverDashboardView.as_view(), name='driver_dashboard'),
     path('packages/<str:pk>/status/', UpdatePackageStatusView.as_view(), name='update_package_status'),
     path('client/dashboard/', ClientDashboardView.as_view(), name='client_dashboard'),
+    path('packages/new/', CreatePackageView.as_view(), name='create_package'),
 ]
