ba80a4ccae53ec4b7b0451ef82d2f05d3068c340
MichalS <D00282723@student.dkit.ie>
2026-08-03 02:45:16 +0200

    feat: creat manager dash template and add url

--- shipments/urls.py
+++ shipments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UpdatePackageStatusView, DriverDashboardView, ClientDashboardView, CreatePackageView
+from .views import UpdatePackageStatusView, DriverDashboardView, ClientDashboardView, CreatePackageView, ManagerDashboardView, AssignDriverView
 urlpatterns = [
     path('driver/dashboard/', DriverDashboardView.as_view(), name='driver_dashboard'),
     path('packages/<str:pk>/status/', UpdatePackageStatusView.as_view(), name='update_package_status'),
     path('client/dashboard/', ClientDashboardView.as_view(), name='client_dashboard'),
     path('packages/new/', CreatePackageView.as_view(), name='create_package'),
+    path('manager/dashboard', ManagerDashboardView.as_view(), name = "manager_dashboard"),
 ]
